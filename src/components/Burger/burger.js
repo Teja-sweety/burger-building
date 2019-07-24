baaf988f39db6e00092452fb6fee1f8414f15e2e
vinay0458 <vinay0458@gmail.com>
2019-07-23 23:25:40 -0700

    adding salad to burger

--- src/components/Burger/burger.js
+++ src/components/Burger/burger.js
@@ -6,6 +6,7 @@ const burger = (props) => {
         <div className="Burger">
             <BurgerIngredient type="bread-top"/>
             <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="salad"/>
             <BurgerIngredient type="cheese"/>
             <BurgerIngredient type="bread-bottom"/>
          </div>
